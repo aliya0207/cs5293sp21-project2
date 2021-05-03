70c93adfea4a756cf8cbc050dc4b7395bf16f01f
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-05-03 13:10:29 -0500

    Create test1.py

---
+++ test/test1.py
@@ -0,0 +1,8 @@
+import pytest
+import main
+
+text = " Daniel Radcliffe was exceptional in the last movie of the series."
+
+def test1():
+    name = main.get_redacted_entity(data)
+    assert name is not None    
