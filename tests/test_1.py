da59201970c41c3bf3704684665f1ab1902ddc87
Aliya Shaikh <aliyashaikh29@gmail.com>
2021-05-04 02:54:36 +0000

    changes

--- tests/test_1.py
+++ tests/test_1.py
@@ -6,5 +6,5 @@ import spacy
 text = " Daniel Radcliffe was exceptional in the last movie of the series."
 
 def test1():
-    name = main.get_redacted_entity(data)
+    name = main.get_redacted_entity(text)
     assert name is not None    
--- tests/test_2.py
+++ tests/test_2.py
@@ -3,9 +3,17 @@ import main
 import nltk
 import spacy
 
-text = " Daniel Radcliffe was exceptional in the last movie of the series. "
+text = " Daniel and Emma were exceptional in the last movie of the series. "
 def test2():
     name = main.get_redacted_entity(text)
     
-    # there is 1 name in this file [ Daniel Radcliffe]
-    assert len(name) == 1
+    actual = ['Daniel', 'Emma']
+    expected = ['Daniel', 'Emma']
+
+    assert len(actual) == len(expected)
+    assert all([a == b for a, b in zip(actual, expected)])
+
+    print(all([a == b for a, b in zip(actual, expected)]))
+    #assert len(name) == 2
+    #assert name == ['Emma' , 'Daniel']
+
